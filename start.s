a9d51b7182ff67925ea4dcc8d8ce9cecefe1938e
Hayden <haydenfreeman@yahoo.com>
2025-03-14 08:28:28 -0500

    Auto commit: Fri Mar 14 08:28:28 AM CDT 2025

---
+++ start.s
@@ -0,0 +1,20 @@
+section .data
+
+section .bss
+
+section .text
+    global _start
+
+_start:
+
+exit0:
+    mov rax, 60
+    mov rdi, 1
+    syscall
+    ret
+exit1:
+    mov rax, 60
+    mov rdi, 1
+    syscall
+    ret
+

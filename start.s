3c24a20d53a60bbc7a5a9aa1e07a93af63abb0f8
Hayden <haydenfreeman@yahoo.com>
2025-03-14 05:26:52 -0500

    Auto commit: Fri Mar 14 05:26:52 AM CDT 2025

---
+++ start.s
@@ -0,0 +1,19 @@
+section .data
+    text db "hello world",10
+    a dd 0
+
+section .text
+    global _start
+
+_start:
+    ; write()
+    mov rax, 1
+    mov rdi, 1
+    mov rsi, text 
+    mov rdx, 11
+    syscall
+
+    ; exit()
+    mov rax, 60
+    mov rdi, 0
+    syscall

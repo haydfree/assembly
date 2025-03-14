fe3e1dbd60a290286b463f61945e2f34b187ffa4
Hayden <haydenfreeman@yahoo.com>
2025-03-14 07:12:52 -0500

    Auto commit: Fri Mar 14 07:12:52 AM CDT 2025

---
+++ start.s
@@ -0,0 +1,47 @@
+section .data
+    nl db 0xA
+    msga db "enter operand a",10
+    msgb db "enter operand b",10
+    msgop db "enter operator",10
+    msgentry db "you entered:",10
+    msganswer db "answer:",10
+
+section .bss
+    a resd 1
+    b resd 1
+    op resb 1
+    result resd 1
+
+section .text
+    global _start
+
+_start:
+
+
+add:
+    mov rax, [a]
+    add rax, [b]
+    mov [result], rax
+    jmp output
+
+sub:
+    mov rax, [a]
+    sub rax, [b]
+    mov [result], rax
+    jmp output
+
+mul:
+    mov eax, [a]
+    mov edx, 0
+    mul dword [b]
+    mov [result], eax
+    jmp output
+
+div:
+    mov eax, [a]
+    mov edx, 0
+    div dword [b]
+    mov [result], eax
+    jmp output
+
+output:

81dfb2af275658145fd0a570cb902a1e0249c578
Hayden <haydenfreeman@yahoo.com>
2025-03-15 07:34:53 -0500

    Auto commit: Sat Mar 15 07:34:53 AM CDT 2025

---
+++ start.s
@@ -0,0 +1,49 @@
+section .data
+    prompt db "enter op1, op2, oper", 0
+    prompt_len dq $ - prompt
+
+section .bss
+    buffer resb 8
+    a resb 8
+
+section .text
+    global _start
+
+_start:
+    mov rsi, prompt
+    mov rdx, [prompt_len]
+    call write
+
+    mov rsi, buffer
+    mov rdx, 32
+    call read
+    mov [a], [buffer]
+    mov rcx, [a]
+    add rcx, 48
+    mov rsi, rcx
+    mov rdx, 8
+    call write
+
+
+    call exit
+
+read:
+    mov rax, 0
+    mov rdi, 0
+    syscall
+    ret
+
+write:
+    mov rax, 1
+    mov rdi, 1
+    syscall
+    ret
+
+exit:
+    mov rax, 60
+    mov rdi, 0
+    syscall
+    ret
+
+
+
